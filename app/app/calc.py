bccedae2cbf8b641256a17f4f39e8857345bf88a
NFC <erolhovis@gmail.com>
2022-12-03 16:32:28 -0500

    Added postgresql db.

---
+++ app/app/calc.py
@@ -0,0 +1,9 @@
+"""Calculator functions
+"""
+def add( x: float, y: float ) -> float:
+    """ Adds 2 numbers and returns the sum """
+    return x + y
+
+def subtract( x: float, y:float) -> float:
+    """ subtract y from x """
+    return x - y
